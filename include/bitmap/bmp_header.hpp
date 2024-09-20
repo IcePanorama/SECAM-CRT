32d614714d1bfd4faaa57c42a820bc3beedea717
IcePanorama <thriftedroot@protonmail.com>
2024-09-20 12:49:39 -0400

    Created BitmapImage class.

--- include/bitmap/bmp_header.hpp
+++ include/bitmap/bmp_header.hpp
@@ -8,14 +8,14 @@
 class BitmapV5Header
 {
   uint32_t header_size;
-  uint32_t img_width;
-  uint32_t img_height;
+  int32_t img_width;
+  int32_t img_height;
   uint16_t num_color_planes;
   uint16_t bits_per_pixel;
   uint32_t compression_method;
   uint32_t img_size;
-  uint32_t x_ppm; // x pixels per meter
-  uint32_t y_ppm; // y pixels per meter
+  int32_t x_ppm; // x pixels per meter
+  int32_t y_ppm; // y pixels per meter
   uint32_t len_color_table;
   uint32_t num_important_colors;
   uint32_t red_bitmask;
@@ -27,7 +27,10 @@ class BitmapV5Header
   uint32_t red_gamma;
   uint32_t green_gamma;
   uint32_t blue_gamma;
-  /// @see https://learn.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-bitmapv5header#members
+  /**
+   *  @see \
+   *    https://learn.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-bitmapv5header#members
+   */
   uint32_t intent;
   uint32_t icc_profile_data;
   uint32_t icc_profile_size;
--- include/bitmap/bmp_image.hpp
+++ include/bitmap/bmp_image.hpp
@@ -1,12 +1,22 @@
 #ifndef _BITMAP_IMAGE_HPP_
 #define _BITMAP_IMAGE_HPP_
 
+#include "bitmap/bmp_header.hpp"
+
 #include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
 
 class BitmapImage
 {
-  uint32_t size;        // in bytes
-  uint32_t data_offset; // offset from beg. of file to bmp data
+  std::string filename;
+  uint32_t size;             // in bytes
+  uint32_t data_offset;      // offset from beg. of file to bmp data
+  BitmapV5Header DIB_header; // DIB = device-independent bitmap
+  std::unique_ptr<std::vector<uint8_t> > raw_data;
+public:
+  BitmapImage (std::string filename);
 };
 
 #endif /* _BITMAP_IMAGE_HPP_ */
